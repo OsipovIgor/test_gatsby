b0a3003881db5ce818d3deb621ac934ae7e0c3df
Igor Osipov <osipov.kaluga@gmail.com>
2020-09-03 03:06:39 +0300

    articles

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: 'q7gsw2bt5qsu',
+        accessToken: 'Ur7DaL5uj16RjQrhpQCF1hWixvdLqzUok7ISq1wquno'
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
